c4549cf4634e56129fc90a03d904ad5c49b1057e
Tobias Fischer <info@tobiasfischer.info>
2025-03-03 08:58:27 +1000

    Update rosidl_typesupport_c_generate_interfaces.cmake
    Signed-off-by: Tobias Fischer <info@tobiasfischer.info>

--- rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
+++ rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
@@ -121,7 +121,6 @@ target_link_libraries(${rosidl_generate_interfaces_TARGET}${_target_suffix} PUBL
   ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c)
 
 target_link_libraries(${rosidl_generate_interfaces_TARGET}${_target_suffix} PRIVATE
-  rosidl_runtime_c::rosidl_runtime_c
   rosidl_typesupport_c::rosidl_typesupport_c
   rosidl_typesupport_interface::rosidl_typesupport_interface)
 
